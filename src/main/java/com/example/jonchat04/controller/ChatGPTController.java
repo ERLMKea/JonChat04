17064fe389ec7f15a644752d8548acbf0a1108a7
erlm <erlm@kea.dk>
2023-10-31 17:31:38 +0100

    Initial commit

---
+++ src/main/java/com/example/jonchat04/controller/ChatGPTController.java
@@ -0,0 +1,53 @@
+package com.example.jonchat04.controller;
+
+import com.example.jonchat04.dto.ChatRequest;
+import com.example.jonchat04.dto.ChatResponse;
+import com.example.jonchat04.dto.Choice;
+import com.example.jonchat04.dto.Message;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ChatGPTController {
+
+    private final WebClient webClient;
+
+    public ChatGPTController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://api.openai.com/v1/chat/completions").build();
+    }
+
+    @GetMapping("/chat")
+    public List<Choice> chatWithGPT(@RequestParam String message) {
+        ChatRequest chatRequest = new ChatRequest();
+        chatRequest.setModel("gpt-3.5-turbo");
+        List<Message> lstMessages = new ArrayList<>();
+        lstMessages.add(new Message("system", "You are a helpful assistant."));
+        lstMessages.add(new Message("user", "Where is Copenhagen"));
+        chatRequest.setMessages(lstMessages);
+        chatRequest.setN(1);
+        chatRequest.setTemperature(1);
+        chatRequest.setMaxTokens(30);
+        chatRequest.setStream(false);
+        chatRequest.setPresencePenalty(1);
+
+        ChatResponse response = webClient.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(h -> h.setBearerAuth("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"))
+                .bodyValue(chatRequest)
+                .retrieve()
+                .bodyToMono(ChatResponse.class)
+                .block();
+
+        List<Choice> lst = response.getChoices();
+
+        return lst;
+
+    }
+
+}
