11864a137c4c37a2185a2988bb650ddd6432fa71
erlm <erlm@kea.dk>
2023-11-01 10:39:33 +0100

    nu med dokumentation

--- src/main/java/com/example/jonchat04/controller/ChatGPTController.java
+++ src/main/java/com/example/jonchat04/controller/ChatGPTController.java
@@ -25,17 +25,17 @@ public class ChatGPTController {
 
     @GetMapping("/chat")
     public List<Choice> chatWithGPT(@RequestParam String message) {
-        ChatRequest chatRequest = new ChatRequest();
-        chatRequest.setModel("gpt-3.5-turbo");
-        List<Message> lstMessages = new ArrayList<>();
+        ChatRequest chatRequest = new ChatRequest(); //ChatRequest objekt har jeg dannet med https://www.jsonschema2pojo.org/ værktøj
+        chatRequest.setModel("gpt-3.5-turbo"); //vælg rigtig model. se powerpoint
+        List<Message> lstMessages = new ArrayList<>(); //en liste af messages med roller
         lstMessages.add(new Message("system", "You are a helpful assistant."));
         lstMessages.add(new Message("user", "Where is Copenhagen"));
         chatRequest.setMessages(lstMessages);
-        chatRequest.setN(1);
-        chatRequest.setTemperature(1);
-        chatRequest.setMaxTokens(30);
-        chatRequest.setStream(false);
-        chatRequest.setPresencePenalty(1);
+        chatRequest.setN(1); //n er antal svar fra chatgpt
+        chatRequest.setTemperature(1); //jo højere jo mere fantasifuldt svar (se powerpoint)
+        chatRequest.setMaxTokens(30); //længde af svar
+        chatRequest.setStream(false); //stream = true, er for viderekomne, der kommer flere svar asynkront
+        chatRequest.setPresencePenalty(1); //noget med ikke at gentage sig. se powerpoint
 
         ChatResponse response = webClient.post()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -48,6 +48,17 @@ public class ChatGPTController {
         List<Choice> lst = response.getChoices();
 
         return lst;
+
+
+        /*ChatResponse response = webClient.post()  , man skal kalde med post
+                .contentType(MediaType.APPLICATION_JSON)  , vigtigt at huske
+                .headers(h -> h.setBearerAuth("sk-kB0mg16RNk3IZlaVdjQ6Sv8Jb9KP4b0")) , sæt openapi key ind. brug evt env variable i application.properties og @Value
+                .bodyValue(chatRequest) her indsættes spørgsmål som er pakket ind i et ChatRequest objekt
+                .retrieve() går igang med at hente
+                .bodyToMono(ChatResponse.class) der kommer json tilbage, og dette json skal bygges om til Java, angiv Java klasse, her vores ChatResponse
+                .block(); her venter vi på at webClient læser restApi og bygger Java
+       */
+
 
     }
 
